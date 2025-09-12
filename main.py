f4d241169fbbc363fde86a0f977e6b24623fb817
SYYnb666 <3638506532@qq.com>
2025-09-12 09:02:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #   WeightConvert.py
-WeightStr = input("请输入带有符号的重量值:")
-if  WeightStr[-1]in ["kg"]:
-    pd = (eval (Weight[0:-1]))*2.20462
-    print("转化后的重量是{：.3f}pd".format(pd))
-elif  WeightStr[-1] in ["pd"]:
-    kg = (eval (weight[0:-1]))*0.453592
-    print("转化后的重量是{:.3f}kg".format(kg))
-else 
-print("输入格式错误")
+WeightStr = 10kg
+WeightStr[-1]in ["kg"]:
+pd = (eval (Weight[0:-1]))*2.20462
+print("转化后的重量是{：.3f}pd".format(pd))
+WeightStr = 10pd
+WeightStr[-1] in ["pd"]:
+kg = (eval (weight[0:-1]))*0.453592
+print("转化后的重量是{:.3f}kg".format(kg))
+ 
                   
 
