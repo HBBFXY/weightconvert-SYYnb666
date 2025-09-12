01d9d2c5d4a83e314eba43094a917bdeef6ff857
SYYnb666 <3638506532@qq.com>
2025-09-12 10:28:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #   WeightConvert.py
 WeightStr = input()
 if  WeightStr[-1]in ["kg"]:
-pd = (eval (Weight[0:-1]))*2.20462
-print("对应的英制重量为{：.3f}pd镑".format(pd))
+    pd = (eval (Weight[0:-1]))*2.20462
+    print("对应的英制重量为{：.3f}pd镑".format(pd))
 elif  WeightStr[-1] in ["pd"]:
-kg = (eval (Weight[0:-1]))*0.453592
-print("对应的公制质量为{:.3f}kg公斤".format(kg))
+    kg = (eval (Weight[0:-1]))*0.453592
+    print("对应的公制质量为{: .3f}kg公斤".format(kg))
+ else  print（）
  
                   
 
