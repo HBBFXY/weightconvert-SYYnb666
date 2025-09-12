bf633e784ea2688a68c0ccb84eb5d4ba7b83beaf
SYYnb666 <3638506532@qq.com>
2025-09-12 09:05:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 WeightStr = 10kg
 WeightStr[-1]in ["kg"]:
 pd = (eval (Weight[0:-1]))*2.20462
-print("转化后的重量是{：.3f}pd".format(pd))
+print("对应的英制重量为{：.3f}pd英镑".format(pd))
 WeightStr = 10pd
 WeightStr[-1] in ["pd"]:
 kg = (eval (weight[0:-1]))*0.453592
-print("转化后的重量是{:.3f}kg".format(kg))
+print("对应的千克质量为{:.3f}kg公斤".format(kg))
  
                   
 
